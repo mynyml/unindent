557a68c78fab215c961f227403d525c4b6df9acc
mynyml <mynyml@gmail.com>
2010-01-08 15:45:38 -0500

    Fix ruby1.9 compatibility

--- lib/unindent.rb
+++ lib/unindent.rb
@@ -1,11 +1,9 @@
-module Unindentation
+class String
   def unindent
-    indent = self.select {|line| !line.strip.empty? }.map {|line| line.index(/[^\s]/) }.compact.min
+    indent = self.split("\n").select {|line| !line.strip.empty? }.map {|line| line.index(/[^\s]/) }.compact.min
     self.gsub(/^[[:blank:]]{#{indent}}/, '')
   end
   def unindent!
     self.replace(self.unindent)
   end
 end
-
-String.class_eval { include Unindentation }
